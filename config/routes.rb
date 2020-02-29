314df8fb8ab1c05836b05c7c23a09aaa74d878f4
ShogoKubota <shogokubota1222@gmail.com>
2020-02-29 15:09:45 +0900

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top/index'
+  root to: 'top#index'
+  resources :health_check, only: [:index]
 end
